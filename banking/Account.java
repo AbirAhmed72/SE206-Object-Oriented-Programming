fc3f77d2cbb2dd701c7dc6a5513a181495b63fd9
Saeed Siddik <saeed.siddik@iit.du.ac.bd>
2020-12-29 10:37:17 +0600

    Include method overload, toString, pin variable

--- banking/Account.java
+++ banking/Account.java
@@ -4,6 +4,7 @@ public class Account {
     private String account_holder_name;
     private String account_pnone_number;
     private double balance;
+    private String pin;
 
 //    constructor
 //    is a function having same name as class
@@ -11,9 +12,38 @@ public class Account {
 //    no return type
 //    can accept multiple agruments as function parameters
 
+//    Constructor having pin, name, phone_number  arguments <String, String, String>
+    public Account(String name, String account_pnone_number, String pin){
+        this.account_holder_name = name;
+        this.account_pnone_number = account_pnone_number;
+        this.pin = pin;
+    }
+
+//    Constructor without pin number  arguments <String, String>
     public Account(String name, String account_pnone_number){
         this.account_holder_name = name;
         this.account_pnone_number = account_pnone_number;
+    }
+
+//    Method overloading  []
+//    Polymorphism
+
+//    update pin; return true if success or return false if failed
+    public boolean pin_update(String old_pin, String new_pin){
+        if(old_pin == this.pin){
+            this.pin = new_pin;
+            System.out.println("Pin updated. " + pin_print());
+            return true;
+        }
+        else{
+            System.out.println("Old pin does not match. Pin update failed. " + pin_print());
+            return false;
+        }
+    }
+
+    private String pin_print(){
+        String new_pin_info = getAccount_holder_name()+ ", Pin: " + this.pin;
+        return new_pin_info;
     }
 
     public void add_money(double amount){
@@ -21,15 +51,28 @@ public class Account {
         System.out.println("Add money: " + amount);
     }
 
-    public void cash_out(double amount){
-        if (balance >= amount){
-            balance -= amount;
-            System.out.println("Cash out: " + amount);
+    public void cash_out(double amount, String pin){
+        if (pin != this.pin){
+            System.out.println("Pin does not match");
+            return;
         }
+        else{
+            if (balance >= amount){
+                balance -= amount;
+                System.out.println("Cash out: " + amount);
+            }
 
-        else {
-            System.out.println("Failed to cash out due to balance limit");
+            else {
+                System.out.println("Failed to cash out due to balance limit");
+            }
         }
+    }
+
+    @Override
+    public String toString() {
+        return "Account { " +
+                "Account Holder Name= " + account_holder_name  +
+                ", Pnone Number= " + account_pnone_number + ", Balance= " + balance + "}";
     }
 
     public String getAccount_holder_name(){
